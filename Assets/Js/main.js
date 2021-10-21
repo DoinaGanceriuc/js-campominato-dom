9bca2b02c299392f4a7732d76b409e8292ac8f16
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-21 13:02:14 +0200

    add details to generate random numbers

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -3,7 +3,19 @@ L'utente indica un livello di difficoltà in base al quale viene generata una gr
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+
+In seguito l'utente clicca su ogni cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando
+il giocatore clicca su una bomba
+o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+*/
 
 
 /* Strumenti
@@ -16,10 +28,11 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 */
 
 // inizializzazione delle variabili
-const chooseLevel = prompt("Scegli il livello di difficoltà[1/2/3]")
-const levelOne = "1";
-const levelTwo = "2"; 
-const levelThree = "3";
+/* const chooseLevel = Number(prompt("Scegli il livello di difficoltà[1/2/3]")) */
+const chooseLevel = 3;
+const levelOne = 1;
+const levelTwo = 2; 
+const levelThree = 3;
 
 // selezionare elementi DOM
 const containerElement = document.querySelector(".container");
@@ -27,7 +40,7 @@ const containerElement = document.querySelector(".container");
 
 /**
  * Questa funzione cicla una serie di elementi in base al valore che riceve in ingresso
- * @param {string} valCiclo - indica il valore da ricevere in input
+ * @param {number} valCiclo - indica il valore da ricevere in input
  */
 function ciclaElementi(valCiclo) {
 
@@ -52,17 +65,37 @@ ciclaElementi(levelPlay());
 
 /**
  * Questa funzione ha lo scopo di definire qual è il livello del gioco e restiuisce una stringa diversa in base alla difficoltà del livello
- * @returns {string} - indica il valore che resituisce se la condizione si verifica o meno
+ * @returns {number} - indica il valore che resituisce se la condizione si verifica o meno
  */
 function levelPlay() {
     // condizione di verifica
     if (chooseLevel == levelOne){
-        return "100"
+        return 100
     } else if (chooseLevel == levelTwo) {
-        return "81"
+        return 81
     } else {
-        return "49"
+        return 49
     }
 }
 // Invocare una funzione
 levelPlay();
+
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+*/
+
+function pcRandomNumbers(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+/* pcRandomNumbers(1, levelPlay()); */
+
+// I numeri nella lista delle bombe non possono essere duplicati.
+const listaNumbers = [];
+
+while (listaNumbers.length < 16) {
+    const randomNumber = pcRandomNumbers(1, levelPlay())
+    if(!listaNumbers.includes(randomNumber)) {
+        listaNumbers.push(randomNumber)
+         console.log(randomNumber);
+    }
+}
+console.log(listaNumbers);

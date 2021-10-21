845be15277e9cd5d4a2865d62da81c41b5b54c73
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-21 21:49:15 +0200

    add function to color all cells

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -31,39 +31,33 @@ const levelThree = 3;
 
 // selezionare elementi DOM
 const containerElement = document.querySelector(".container");
+const singolaCellaElement = document.getElementsByClassName("cella")
+/* console.log(singolaCellaElement); */
 
 
 /**
  * Questa funzione cicla una serie di elementi in base al valore che riceve in ingresso
  * @param {number} valCiclo - indica il valore da ricevere in input
  */
-let cellaElement;
 let eventoClick = false;
 function ciclaElementi(valCiclo) {
 
     //ciclo for
     for (let i = 1; i <= valCiclo; i++) { 
-    cellaElement = document.createElement("div");
-    cellaElement.className = "cella";
-    cellaElement.innerHTML = i;
-    containerElement.insertAdjacentElement("beforeend", cellaElement)
+    let cellaElement = document.createElement("div");
+    cellaElement.append(i)
+    cellaElement.classList.add("cella");
+    containerElement.append(cellaElement);
+    /* console.log(cellaElement); */
 
     // evento sul click
-    cellaElement.addEventListener("click",addListen)
-
-
-}  
-}
-function addListen() {
-
-/* In seguito l'utente clicca su ogni cella:
-se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
-altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.   */      
+    cellaElement.addEventListener("click",function () {
     const valoreCella = Number(this.innerText);
-    this.style.backgroundColor = "lightBlue";
-    if (listaNumbers.includes(valoreCella)) {
+    /* this.style.backgroundColor = "blue"; */
+  if (bombs.includes(valoreCella)) {
         console.log("bomba");
         this.style.backgroundColor = "red";
+        alert("hai perso")
         eventoClick = true;
         console.log(eventoClick);
         
@@ -72,11 +66,25 @@ altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a c
         this.style.backgroundColor = "lightBlue";
     }
 
+    })
 
 
 
-    
-    }
+
+
+}  
+}
+
+
+/* In seguito l'utente clicca su ogni cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.   */      
+   
+
+/*     if (eventoClick == true) {
+    console.log(cellaElement.removeEventListener("click", addListen));;
+} */
+
 
 // Invocare una funzione
 ciclaElementi(levelPlay());
@@ -96,7 +104,8 @@ function levelPlay() {
     }
 }
 // Invocare una funzione
-levelPlay();
+const livelloDifficolta = levelPlay();
+/* console.log(livelloDifficolta); */
 
 /* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 */
@@ -107,17 +116,23 @@ function pcRandomNumbers(min, max) {
 /* pcRandomNumbers(1, levelPlay()); */
 
 // I numeri nella lista delle bombe non possono essere duplicati.
-const listaNumbers = [];
-let randomNumber
 
-while (listaNumbers.length < 16) {
-    randomNumber = pcRandomNumbers(1, levelPlay())
+function getPcListRandomNumbers(range) {
+    const listaNumbers = [];
+
+    while (listaNumbers.length < 16) {
+    const randomNumber = pcRandomNumbers(1, range)
     if(!listaNumbers.includes(randomNumber)) {
         listaNumbers.push(randomNumber)
          console.log(randomNumber);
     }
 }
-console.log(listaNumbers);
+return listaNumbers;
+}
+
+const bombs = getPcListRandomNumbers(livelloDifficolta)
+
+
 
 /* La partita termina quando
 il giocatore clicca su una bomba
@@ -125,17 +140,61 @@ o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito (16).
 */
 
-// se hai cliccato sulla bomba hai perso
-// se hai cliccato per 16 volte e non hai mai preso una bomba hai vinto
-// fai vedere tutti i numeri dell'array al termine
 
+/* function removeClick () {
 
-if (eventoClick == true) {
-    console.log("qualcosa");
-  /*  console.log(cellaElement.removeEventListener("click", addListen));  */
+   cellaElement.removeEventListener("click", addListen);
 
-
-    
 }
 
 
+removeClick()
+ */
+
+// ciclare tra le celle
+
+function coloraCelle()  {
+    for (let index = 0; index < singolaCellaElement
+    .length; index++) {
+    let singolaCella = singolaCellaElement[index];
+    console.log(singolaCella);
+    let numeroCella = parseInt(singolaCella.textContent);
+    console.log(numeroCella);
+
+     
+       /*   verifyBomb(numeroCella) */
+     if (bombs.includes(numeroCella)) {
+        singolaCella.style.backgroundColor = "red";
+        return singolaCella
+    }
+}
+    
+
+/*  coloraCelle()   */
+   if (eventoClick == true) {
+       coloraCelle()
+   }
+
+
+    
+    // element.addEventListener("click", handleClick() 
+        // prendere il contenuto della cella
+       /*  const cellNumber = parseInt(this.innerText)
+        console.log(cellNumber);
+        is_a_bomb(cellNumber, bombs) */
+
+    //)
+
+}
+
+function verifyBomb(valoreCella){
+
+    if (bombs.includes(valoreCella)) {
+        console.log("bomba");
+        /* valoreCella.classList.add() = "red"; */
+        
+    } else {
+        console.log("continua");
+        /* valoreCella.classList.add() = "blue"; */
+    }
+}
